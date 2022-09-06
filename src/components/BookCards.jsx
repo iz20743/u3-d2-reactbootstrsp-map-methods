38a5cc95a8e3bfdcd1c2459f9f6c969b59d830cb
iz20743 <93397767+iz20743@users.noreply.github.com>
2022-09-06 14:56:41 +0100

    bookCards and Navbar

---
+++ src/components/BookCards.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+
+class Bookcards extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+
+        );
+    }
+
+}
+
+export default Bookcards
---
+++ src/components/CustomNavbar.jsx
@@ -0,0 +1,47 @@
+import { Component } from "react";
+import{Container, Navbar, NavDropdown, Nav,} from 'react-bootstrap';
+
+
+class CustomNavbar extends Component{
+    render(){
+        return(
+            <div>
+                <Navbar bg="light" expand="lg">
+                    <Container fluid>
+                        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="me-auto my-2 my-lg-0"
+                                style={{ maxHeight: '100px' }}
+                                navbarScroll
+                            >
+                                <Nav.Link href="#action1">Home</Nav.Link>
+                                <NavDropdown title="GENER" id="navbarScrollingDropdown">
+                                    <NavDropdown.Item href="#action3">FANTASY</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action4">
+                                        HISTORY
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action5">
+                                        ROMANCE
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item href="#action6">
+                                        SIFI
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                                <Nav.Link href="#" disabled>
+                                Link
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+
+        );
+    }
+
+}
+
+export default CustomNavbar
