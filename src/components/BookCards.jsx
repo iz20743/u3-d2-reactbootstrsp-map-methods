45d9ee23db3efb8b5a170069bbd6b37a2d13c2ed
iz20743 <93397767+iz20743@users.noreply.github.com>
2022-09-06 15:53:49 +0100

    bookCards

--- src/components/BookCards.jsx
+++ src/components/BookCards.jsx
@@ -1,16 +1,36 @@
 import { Component } from "react";
+import{Card, Button, Row, Container, Col,} from 'react-bootstrap';
+import arrayOfBooks from '../data/books/fantasy.json'
 
 
-class Bookcards extends Component{
+class BookCards extends Component{
     render(){
         return(
-            <div>
-                
+            <Container fluid className="mt-5">
+                <h1 className="">Fantasy Books</h1>
+                <div>
+                    <Row>
+                        <Col sm={6} md={3} lg={4}>
+                            <Card  style={{ width: '18rem'}}>
+                                <Card.Img variant="top" src= "http://placekitten.com/200/300" />
+                                <Card.Body>
+                                    <Card.Title>Card Title</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary">BUY</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
             </div>
+                
+            </Container>
 
         );
     }
 
 }
 
-export default Bookcards
+export default BookCards
